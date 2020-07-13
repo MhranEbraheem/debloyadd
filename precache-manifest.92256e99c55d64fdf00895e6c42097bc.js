9ab08affb4b3eba140e91af7e367cf97ea1ca4d2
MhranEbraheem <61749759+MhranEbraheem@users.noreply.github.com>
2020-07-13 16:18:07 +0300

    Updates

---
+++ precache-manifest.92256e99c55d64fdf00895e6c42097bc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f156a280cfd25f0de3074c79df79ec8c",
+    "url": "/debloyadd/index.html"
+  },
+  {
+    "revision": "068c343c8a8b9b9ce5d6",
+    "url": "/debloyadd/static/css/main.28695900.chunk.css"
+  },
+  {
+    "revision": "00771b1a13066626ba1d",
+    "url": "/debloyadd/static/js/2.caa915cd.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/debloyadd/static/js/2.caa915cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "068c343c8a8b9b9ce5d6",
+    "url": "/debloyadd/static/js/main.81e5ec0c.chunk.js"
+  },
+  {
+    "revision": "94eb22ebe0e3e9e35263",
+    "url": "/debloyadd/static/js/runtime-main.1ec78f83.js"
+  },
+  {
+    "revision": "8be21d15abc38aa6ea5360984c8295a6",
+    "url": "/debloyadd/static/media/airpods.8be21d15.png"
+  },
+  {
+    "revision": "69764619239a7fb8a5dcb5d21e44f06a",
+    "url": "/debloyadd/static/media/cam.69764619.png"
+  },
+  {
+    "revision": "9fa2669f5b82168dc156c05afe32c07d",
+    "url": "/debloyadd/static/media/pc.9fa2669f.png"
+  },
+  {
+    "revision": "b8e6ada8afd4e813e6d23533cf37c223",
+    "url": "/debloyadd/static/media/phone.b8e6ada8.png"
+  }
+]);
